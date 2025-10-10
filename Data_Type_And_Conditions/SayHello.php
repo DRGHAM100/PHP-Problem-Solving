5e7eacd322dfc9328ee52e93603389d1b426f0a7
Drgham Dakhol <=>
2025-10-10 22:01:29 +0400

    Say Hello

---
+++ Data_Type_And_Conditions/SayHello.php
@@ -0,0 +1,23 @@
+<?php
+/*
+    Problem: Say Hello
+
+    Description:
+    Given a name S, print "Hello, <name>" without quotes.
+
+    Input:
+    - Only one line containing a string S.
+
+    Output:
+    - Print "Hello, " followed by the name.
+
+    Example:
+    Input: Drgham
+    Output: Hello, Drgham
+*/
+
+// Read input
+$name = readline();
+
+// Print output
+echo "Hello, " . $name . PHP_EOL;
